c79e9837b21d05ff2042324ca2a1beb0ef4eeb9d
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-11-04 18:55:01 -0500

    adds tool to pull commits on a repo since a given date

---
+++ PullCommitsSinceRelease.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+require('dotenv').config();
+
+const user = 'ceceliacreates';
+const repo = 'project-green';
+const startDate = '2019-05-06T00:00:00Z';
+const commitMessages = [];
+
+fetch(`https://api.github.com/repos/${user}/${repo}/commits?sha=${process.env.SHA}&per_page=100&since=${startDate}`, {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json', 'Authorization': `${process.env.TOKEN}` }
+})
+    .then(res => res.json())
+    .then(json => {
+        json.forEach(commit => commitMessages.push({ 'message': commit.commit.message, 'date': commit.commit.author.date }));
+        
+        fs.writeFile('./commits.json', JSON.stringify(commitMessages), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!')});
+        console.log(commitMessages);
+        console.log(commitMessages.length)
+    });
